25fa81266a5a3fbe31b3c3dcb826bb2da61e2717
queenn31 <queenn31@gmail.com>
2022-09-12 19:23:45 +0300

    num1, num2, num3

---
+++ num3/Program.cs
@@ -0,0 +1,6 @@
+﻿int num = 2;
+for (int i=0; i<50; i++)
+{
+    Console.WriteLine(num);
+    num = (-(~num)); num = (-(~num));
+}
---
+++ num4/Program.cs
@@ -0,0 +1,12 @@
+﻿int i;
+Console.Write("Введите число : ");
+i = int.Parse(Console.ReadLine());
+if (i % 2 == 0)
+{
+Console.Write("Введенное число является четным");
+Console.Read();
+}
+else
+Console.WriteLine("Введенное число нечетное");
+ 
+Console.Read();
